1445892832fc9d8cc348cf6867c5964db65786ae
londonbrown <londonchaim@gmail.com>
2025-03-07 19:19:18 -0800

    Configure a CodePipeline with a Source and Build stage

--- lib/blog-cdk-stack.ts
+++ lib/blog-cdk-stack.ts
@@ -1,17 +1,63 @@
 import * as cdk from "aws-cdk-lib"
+import * as codebuild from "aws-cdk-lib/aws-codebuild"
+import * as codepipeline from "aws-cdk-lib/aws-codepipeline"
+import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions"
+import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager"
 import { Construct } from "constructs"
-
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BlogCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    // The code that defines your stack goes here
+    // Fetch the GitHub token from AWS Secrets Manager
+    const githubToken = secretsmanager.Secret.fromSecretNameV2(this, "GitHubToken", "GitHubToken")
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BlogCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define CodePipeline
+    const pipeline = new codepipeline.Pipeline(this, "ChaimCodesBlogPipeline", {
+      pipelineName: "ChaimCodesBlogPipeline"
+    })
+
+    // Add GitHub as the Source
+    const sourceOutput = new codepipeline.Artifact()
+    pipeline.addStage({
+      stageName: "Source",
+      actions: [
+        new codepipeline_actions.GitHubSourceAction({
+          actionName: "GitHubSource",
+          owner: "londonbrown",
+          repo: "blog-cdk",
+          branch: "main",
+          oauthToken: githubToken.secretValue,
+          output: sourceOutput
+        })
+      ]
+    })
+
+    // Create a simple CodeBuild project
+    const buildProject = new codebuild.PipelineProject(this, "SimpleBuildProject", {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: "0.2",
+        phases: {
+          build: {
+            commands: ['echo "Hello from CodeBuild!"']
+          }
+        }
+      }),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_6_0
+      }
+    })
+
+    // Add "Build" stage to Pipeline
+    pipeline.addStage({
+      stageName: "Build",
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: "RunBuild",
+          project: buildProject,
+          input: sourceOutput
+        })
+      ]
+    })
   }
 }

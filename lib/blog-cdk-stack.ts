35c933429192accc1d630780bd7805c1fb8dbc14
londonbrown <londonchaim@gmail.com>
2025-03-08 23:11:01 -0800

    Add GitHub OIDC Provider

--- lib/blog-cdk-stack.ts
+++ lib/blog-cdk-stack.ts
@@ -1,8 +1,16 @@
 import * as cdk from "aws-cdk-lib"
+import * as iam from "aws-cdk-lib/aws-iam"
 import { Construct } from "constructs"
 
 export class BlogCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    // Define GitHub OIDC Provider
+    const oidcProvider = new iam.OpenIdConnectProvider(this, "GitHubOIDCProvider", {
+      url: "https://token.actions.githubusercontent.com",
+      clientIds: ["sts.amazonaws.com"]
+    })
+
   }
 }

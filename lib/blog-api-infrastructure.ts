109f198f6fd85662db9c92ee3574d95b3809b01c
londonbrown <londonchaim@gmail.com>
2025-03-09 07:42:41 -0700

    fix: Give get-post API permission to query BlogPosts table

--- lib/blog-api-infrastructure.ts
+++ lib/blog-api-infrastructure.ts
@@ -1,4 +1,5 @@
 import * as cdk from "aws-cdk-lib"
+import * as iam from "aws-cdk-lib/aws-iam"
 import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as apigateway from "aws-cdk-lib/aws-apigateway"
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
@@ -45,6 +46,19 @@ export class BlogAPIInfrastructure extends cdk.Stack {
 
     const postRoot = api.root.addResource("post")
 
+    const getPostRole = new iam.Role(this, `GetPostLambdaRole${stage}`, {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")
+      ]
+    })
+    getPostRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ["dynamodb:GetItem"],
+        resources: [blogPostsTable.tableArn]
+      })
+    )
+
     const lambdaConfigs = [
       {
         name: "HelloWorld",
@@ -59,6 +73,7 @@ export class BlogAPIInfrastructure extends cdk.Stack {
         path: "{id}",
         method: "GET",
         zipFile: "lambdas/get-post.zip",
+        role: getPostRole,
         environment: {
           BLOG_POSTS_TABLE: blogPostsTable.tableName
         }
@@ -67,6 +82,7 @@ export class BlogAPIInfrastructure extends cdk.Stack {
 
     lambdaConfigs.forEach((config) => {
       const lambdaFunction = new lambda.Function(this, `${config.name}Lambda${stage}`, {
+        role: config.role,
         runtime: lambda.Runtime.PROVIDED_AL2023,
         handler: "bootstrap",
         code: lambda.Code.fromAsset(config.zipFile),

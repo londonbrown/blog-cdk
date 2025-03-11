0cddc3ff237219ed7eab3dccb3ae4b437bb3402e
londonbrown <londonchaim@gmail.com>
2025-03-10 21:02:24 -0700

    fix: define env at the stack level

--- bin/blog-cdk.ts
+++ bin/blog-cdk.ts
@@ -5,21 +5,13 @@ import { BlogAPIInfrastructure, BlogAPIStage } from "../lib/blog-api-infrastruct
 import { BlogGitHubStagingInfrastructure } from "../lib/blog-git-hub-staging-infrastructure"
 
 const app = new cdk.App()
-new BlogGitHubStagingInfrastructure(app, "BlogGitHubStagingInfrastructure", {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-})
+const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+new BlogGitHubStagingInfrastructure(app, "BlogGitHubStagingInfrastructure", { env })
 new BlogAPIInfrastructure(app, "BlogAPIInfrastructureBeta", {
-  stage: BlogAPIStage.BETA
+  stage: BlogAPIStage.BETA,
+  env
 })
 new BlogAPIInfrastructure(app, "BlogAPIInfrastructureProd", {
-  stage: BlogAPIStage.PROD
+  stage: BlogAPIStage.PROD,
+  env
 })

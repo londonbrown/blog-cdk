65d4d0dbae59a09867bbb3f4e4a0f87311a2eba4
londonbrown <londonchaim@gmail.com>
2025-03-15 04:30:46 -0700

    add: cognito client id to cognito-authorizer

--- lib/blog-api-infrastructure/lambdas/cognito-authorizer.ts
+++ lib/blog-api-infrastructure/lambdas/cognito-authorizer.ts
@@ -1,6 +1,7 @@
 import { Stack } from "aws-cdk-lib"
 import * as apigateway from "aws-cdk-lib/aws-apigateway"
 import { UserPool } from "aws-cdk-lib/aws-cognito"
+import * as cognito from "aws-cdk-lib/aws-cognito"
 import * as lambda from "aws-cdk-lib/aws-lambda"
 import { Construct } from "constructs"
 
@@ -10,14 +11,16 @@ export function createCognitoAuthorizerLambda(
   scope: Construct,
   stage: string,
   userPool: UserPool,
-  apiGateway: apigateway.RestApi
+  apiGateway: apigateway.RestApi,
+  userPoolClient: cognito.UserPoolClient
 ): lambda.Function {
   return createLambdaFunction(scope, `CognitoAuthorizerLambda${stage}`, {
     lambdaPath: "lambdas/cognito-authorizer.zip",
     environment: {
       USER_POOL_ID: userPool.userPoolId,
       AWS_ACCOUNT_ID: Stack.of(scope).account,
-      API_GATEWAY_ID: apiGateway.restApiId
+      API_GATEWAY_ID: apiGateway.restApiId,
+      COGNITO_CLIENT_ID: userPoolClient.userPoolClientId
     }
   })
 }

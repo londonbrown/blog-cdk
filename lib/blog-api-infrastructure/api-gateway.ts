c1ff2984693a95777936d9d268c3e87490648859
londonbrown <londonchaim@gmail.com>
2025-04-13 08:41:27 -0700

    Revert "change: define media types on api"
    This reverts commit 1c5e5a4c6c347b4a10bfdc5d4dc9b4af724ca158.

--- lib/blog-api-infrastructure/api-gateway.ts
+++ lib/blog-api-infrastructure/api-gateway.ts
@@ -33,8 +33,7 @@ export function setupApiGateway(
 ): apigateway.RestApi {
   const api = new apigateway.RestApi(scope, `BlogAPIGateway${stage}`, {
     restApiName: `Blog API (${stage})`,
-    description: "API Gateway for the blog service",
-    binaryMediaTypes: ["image/*", "application/octet-stream", "application/json", "text/*"]
+    description: "API Gateway for the blog service"
   })
 
   const customDomain = new apigateway.DomainName(scope, `BlogAPIDomain${stage}`, {
